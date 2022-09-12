bf4aea1b062e5cb0de337aa857f686c599775950
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-12 11:59:42 +0200

    Add project files.

---
+++ zadatakbr4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatakbr4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            start:Console.WriteLine("Unesi x");
+            double x =  Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesi y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesi z");
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            if(x <= 0 || y <= 0 || z <= 0)
+            {
+                Console.WriteLine("broj nesmije biti manji od nule!");
+                goto start;
+            }
+
+            if(Math.Pow(z, 2) == Math.Pow(y, 2) + Math.Pow(x, 2))
+            {
+                Console.WriteLine("To je pravokutni trokut");
+            }
+            else
+            {
+                Console.WriteLine("To nije pravokutni trokut");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
